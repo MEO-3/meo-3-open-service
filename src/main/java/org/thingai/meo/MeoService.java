e039d68c51ac3e52a794bf1d898fb72d1fcbd55b
lgthvinh <everwellmax@gmail.com>
2025-08-27 01:46:52 +0700

    implement new DAO

--- src/main/java/org/thingai/meo/MeoService.java
+++ src/main/java/org/thingai/meo/MeoService.java
@@ -1,6 +1,9 @@
 package org.thingai.meo;
 
 import org.thingai.base.Service;
+import org.thingai.base.dao.Dao;
+import org.thingai.base.dao.DaoFile;
+import org.thingai.base.dao.DaoSqlite;
 import org.thingai.meo.handlers.MeoHandlerDevice;
 import org.thingai.meo.handlers.MeoHandlerFlow;
 import org.thingai.meo.handlers.MeoHandlerService;
@@ -12,6 +15,16 @@ public class MeoService extends Service {
 
     @Override
     public void onServiceInit() {
+        Dao daoSqlite = new DaoSqlite(appDir + "/êmo.db");
+        Dao daoFile = new DaoFile(appDir + "/data");
+
+        daoSqlite.initDao(new Class[]{
+            // Define your entity classes here
+        });
+
+        daoFile.initDao(new Class[]{
+            // Define your entity classes here
+        });
 
     }
 
