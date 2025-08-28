39b315bff0d7883cc21e9573adf911114b2c210d
lgthvinh <everwellmax@gmail.com>
2025-08-28 20:40:02 +0700

    refactor callback

--- src/main/java/org/thingai/meo/callbacks/MeoDeviceConfigCallback.java
+++
@@ -1,8 +0,0 @@
-package org.thingai.meo.callbacks;
-
-import org.thingai.meo.entities.MeoDevice;
-
-public interface MeoDeviceConfigCallback {
-    void onDeviceConfigUpdated(MeoDevice device);
-    void onDeviceConfigError(String error);
-}
