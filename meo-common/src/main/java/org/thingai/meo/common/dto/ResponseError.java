e787db3a72c293330770da95ed7da6b9fff5d869
lgthvinh <everwellmax@gmail.com>
2026-01-18 14:52:19 +0700

    feat: add ResponseError DTO for error detail handling in API responses

---
+++ meo-common/src/main/java/org/thingai/meo/common/dto/ResponseError.java
@@ -0,0 +1,13 @@
+package org.thingai.meo.common.dto;
+
+public class ResponseError {
+    private String detail;
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
