fce8e43ac0c4f5a12e7da269672ab0f4905b9adc
lgthvinh <everwellmax@gmail.com>
2026-01-22 02:28:56 +0700

    feat: add productId field with getter and setter to MDeviceConfig

--- meo-common/src/main/java/org/thingai/meo/common/entity/device/MDeviceConfig.java
+++ meo-common/src/main/java/org/thingai/meo/common/entity/device/MDeviceConfig.java
@@ -1,4 +1,13 @@
 package org.thingai.meo.common.entity.device;
 
-    public abstract class MDeviceConfig {
+public abstract class MDeviceConfig {
+    protected String productId;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
 }
