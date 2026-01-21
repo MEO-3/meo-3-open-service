5fb790cbeaf0483238b5205975ceebd2b7dbeabb
lgthvinh <everwellmax@gmail.com>
2026-01-22 02:32:10 +0700

    refactor: remove productId field and its accessors from MDeviceConfig

--- meo-common/src/main/java/org/thingai/meo/common/entity/device/MDeviceConfig.java
+++ meo-common/src/main/java/org/thingai/meo/common/entity/device/MDeviceConfig.java
@@ -1,13 +1,4 @@
 package org.thingai.meo.common.entity.device;
 
 public abstract class MDeviceConfig {
-    protected String productId;
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
 }
