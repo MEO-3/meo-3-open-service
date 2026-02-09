d3b4fb463ba23c88ef979ced45a506c1f5b6cc87
lgthvinh <everwellmax@gmail.com>
2026-02-10 00:48:24 +0700

    feat: add RequestDeviceUpsert and ResponseDevice classes with productId field

---
+++ meo-common/src/main/java/org/thingai/meo/common/dto/device/ResponseDevice.java
@@ -0,0 +1,31 @@
+package org.thingai.meo.common.dto.device;
+
+public class ResponseDevice {
+    private String id;
+    private String label;
+    private String productId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
