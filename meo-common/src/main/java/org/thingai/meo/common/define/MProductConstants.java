d132b1027c880834155bbb98e366afc39d48d74b
lgthvinh <everwellmax@gmail.com>
2026-02-10 00:54:59 +0700

    feat: add MProductConstants class with generic product ID constant

---
+++ meo-common/src/main/java/org/thingai/meo/common/define/MProductConstants.java
@@ -0,0 +1,6 @@
+package org.thingai.meo.common.define;
+
+public class MProductConstants {
+    // 12 bytes product id
+    public static final String MEO_GENERIC_PRODUCT_HEX_ID = "4D454F47454E455249430000"; // MEOGENERIC\0\0
+}
