b3e6a7f84d0edfe4170ca11a733d38097698c398
lgthevinh <everwellmax@gmail.com>
2026-06-10 17:26:15 +0700

    feat: add placeholder methods

--- src/main/java/org/thingai/app/meo/handler/MeoDeviceHandler.java
+++ src/main/java/org/thingai/app/meo/handler/MeoDeviceHandler.java
@@ -1,4 +1,23 @@
 package org.thingai.app.meo.handler;
 
+import org.thingai.app.meo.entity.MeoDevice;
+import org.thingai.app.meo.entity.MeoDeviceProfile;
+import org.thingai.base.dao.Dao;
+
 public class MeoDeviceHandler {
+    private static final String TAG = "MeoDeviceHandler";
+
+    private final Dao dao;
+
+    public MeoDeviceHandler(Dao dao) {
+        this.dao = dao;
+    }
+
+    public MeoDeviceProfile getDeviceProfile(String deviceId) {
+        return null;
+    }
+
+    public MeoDevice getDevice(String deviceId) {
+        return null;
+    }
 }
